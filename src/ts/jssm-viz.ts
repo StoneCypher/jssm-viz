9b63f8bf795754d83d98c9895f85dd92a9073c1a
John Haugeland <stonecypher@gmail.com>
2018-12-10 12:53:17 -0800

    Well now it builds, but node immediately crashes

--- src/ts/jssm-viz.ts
+++ src/ts/jssm-viz.ts
@@ -1,6 +1,12 @@
 
-//import vizjs from 'viz.js';
-// const { Module, render } = require('viz.js/lite.render.js');
+import Viz from 'viz.js';
+import { Module, render } from 'viz.js/full.render.js';
+
+var viz = new Viz({ Module, render });
+
+    viz.renderSVGElement('digraph { a -> b; }')
+       .then( element => document.body.appendChild(element) );
+
 
 
 
