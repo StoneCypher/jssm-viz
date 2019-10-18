731ad703073c4c58b60f3537dd8bd6efe0017f3f
John Haugeland <stonecypher@gmail.com>
2019-10-18 02:34:26 -0700

    testing fixed

--- src/ts/tests/general.js
+++ src/ts/tests/general.js
@@ -1,16 +1,17 @@
 
 import {test, describe} from 'ava-spec';
 
-const jssm = require('../../../build/jssm-viz.cjs.js');
+// nyc isn't able to mark something this large, it seems
+// const jssm = require('../../../build/jssm-viz.cjs.js');
+
+// save this setup for OS calls later at least
 
 
 
 
 
 describe('stub/0', async it => {
-
   it('2+2 is 4', t => t.deepEqual(4, 2+2));
-
 });
 
 // stochable
