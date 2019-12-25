07d52bd9cb16aecac36511e130c712e9cadbd836
John Haugeland <stonecypher@gmail.com>
2019-12-24 16:51:18 -0800

    repair dot_to_svg/1, rename but synonymize dot/1 to machine_to_dot/1

--- src/ts/jssm-viz.ts
+++ src/ts/jssm-viz.ts
@@ -2,102 +2,20 @@
 import Viz from 'viz.js';
 import { Module, render } from 'viz.js/full.render.js';
 
-// var viz = new Viz({ Module, render });
-
-//     viz.renderSVGElement('digraph { a -> b; }')
-//        .then( element => document.body.appendChild(element) );
+import { default_viz_colors } from './default_colors';
 
 
 
 
 
-const default_viz_colors = {
-
-  'graph_bg_color'       : '#eeeeee',
-
-  'fill_final'           : '#ddddff',
-  'fill_terminal'        : '#ffdddd',
-  'fill_complete'        : '#ddffdd',
-
-
-  'legal_1'              : '#888888',
-  'legal_2'              : '#777777',
-  'legal_solo'           : '#777777',
-
-  'legal_final_1'        : '#7777aa',
-  'legal_final_2'        : '#666699',
-  'legal_final_solo'     : '#666699',
-
-  'legal_terminal_1'     : '#aa7777',
-  'legal_terminal_2'     : '#996666',
-  'legal_terminal_solo'  : '#996666',
-
-  'legal_complete_1'     : '#77aa77',
-  'legal_complete_2'     : '#669966',
-  'legal_complete_solo'  : '#669966',
-
-
-  'main_1'               : '#444444',
-  'main_2'               : '#333333',
-  'main_solo'            : '#333333',
-
-  'main_final_1'         : '#333366',
-  'main_final_2'         : '#222255',
-  'main_final_solo'      : '#222255',
-
-  'main_terminal_1'      : '#663333',
-  'main_terminal_2'      : '#552222',
-  'main_terminal_solo'   : '#552222',
-
-  'main_complete_1'      : '#336633',
-  'main_complete_2'      : '#225522',
-  'main_complete_solo'   : '#225522',
-
-
-  'forced_1'             : '#cccccc',
-  'forced_2'             : '#bbbbbb',
-  'forced_solo'          : '#bbbbbb',
-
-  'forced_final_1'       : '#bbbbee',
-  'forced_final_2'       : '#aaaadd',
-  'forced_final_solo'    : '#aaaadd',
-
-  'forced_terminal_1'    : '#eebbbb',
-  'forced_terminal_2'    : '#ddaaaa',
-  'forced_terminal_solo' : '#ddaaaa',
-
-  'forced_complete_1'    : '#bbeebb',
-  'forced_complete_2'    : '#aaddaa',
-  'forced_complete_solo' : '#aaddaa',
-
-
-  'text_final_1'         : '#000088',
-  'text_final_2'         : '#000088',
-  'text_final_solo'      : '#000088',
-
-  'text_terminal_1'      : '#880000',
-  'text_terminal_2'      : '#880000',
-  'text_terminal_solo'   : '#880000',
-
-  'text_complete_1'      : '#007700',
-  'text_complete_2'      : '#007700',
-  'text_complete_solo'   : '#007700'
-
-}
+var viz = new Viz({ Module, render });
 
 
 
 
-
-// apparently this is how you get at global.Viz
-// declare var Viz: Function;
 
 function dot_to_svg(dot: string, config? : Object): string {  // whargarbl jssm isn't an any
-//  return vizjs(dot, config);
-  // if (Viz) {
-  //   return Viz(dot, config);
-  // }
-  throw new TypeError('No global viz');
+  return viz.renderString(dot);
 }
 
 
@@ -123,7 +41,9 @@ return 'todo';
 
 
 
-function dot(jssm: any) {  // whargarbl jssm isn't an any
+function dot(jssm: any) { machine_to_dot(jssm); }  // compatability, remove in 2.0.0
+
+function machine_to_dot(jssm: any) {  // whargarbl jssm isn't an any
 
   const l_states = jssm.states();
 
@@ -259,5 +179,5 @@ function dot(jssm: any) {  // whargarbl jssm isn't an any
 
 export {
   dot, dot_to_svg, svg_el,
-//  Viz, Module, render
+  machine_to_dot
 };
