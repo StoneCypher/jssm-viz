68c8da195f95ef755d7c55a58f10f3d08e58a414
John Haugeland <stonecypher@gmail.com>
2024-07-06 10:40:27 -0700

    checkpoint before version fixes

--- src/ts/generated_code/version.ts
+++ src/ts/generated_code/version.ts
@@ -1,5 +1,5 @@
 
-const version    : string = "5.85.9",
-      build_time : number = 1663369998381;
+const version    : string = "5.98.2",
+      build_time : number = 1720287523065;
 
 export { version, build_time };
